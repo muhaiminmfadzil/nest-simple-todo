fb101011ac80e4fb1950c3a9e193fb8b6456dac6
muhaimin <muhaiminmfadzil@gmail.com>
2020-11-05 21:28:10 +0800

    query all todos : done

---
+++ src/todos/todos.resolver.ts
@@ -0,0 +1,18 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { TodosTypedef } from './todos.typedef';
+
+interface TodosInterface {
+  id: number;
+  title: string;
+  descriptions: string;
+}
+
+@Resolver(of => TodosTypedef)
+export class TodosResolver {
+  private readonly todos: TodosInterface[] = [];
+
+  @Query(returns => [TodosTypedef])
+  getTodos(): TodosInterface[] {
+    return this.todos;
+  }
+}
