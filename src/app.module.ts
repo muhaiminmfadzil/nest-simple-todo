789842a7ba2e62bc5f9c6afd8c99cb56d4790b11
muhaimin <muhaiminmfadzil@gmail.com>
2020-11-05 21:06:13 +0800

    gql setting root

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      autoSchemaFile: join(process.cwd(), 'src/gqlSchema.gql'),
+    }),
+  ],
 })
 export class AppModule {}
