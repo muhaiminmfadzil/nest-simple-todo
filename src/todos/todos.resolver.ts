fb272ee8c85bb92c6e709bad5fdb21bfdff7a263
muhaimin <muhaiminmfadzil@gmail.com>
2020-11-05 21:30:30 +0800

    adding description to query : done

--- src/todos/todos.resolver.ts
+++ src/todos/todos.resolver.ts
@@ -11,7 +11,7 @@ interface TodosInterface {
 export class TodosResolver {
   private readonly todos: TodosInterface[] = [];
 
-  @Query(returns => [TodosTypedef])
+  @Query(returns => [TodosTypedef], { description: 'Get all todo lists' })
   getTodos(): TodosInterface[] {
     return this.todos;
   }
